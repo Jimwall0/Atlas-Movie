5b9baaa108a1f6fd03ae3af814f50579f8146ed1
Tsimmuaj Yang <tyang4204@gmail.com>
2026-03-19 15:25:12 +0000

    devcontainer forward ports

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,3 +1,7 @@
 export default function App() {
-  return <p>Hello, World!</p>
+  return (
+    {
+      
+    }
+  )
 }
---
+++ my-app/src/components/loginButton.tsx
@@ -0,0 +1,7 @@
+export function loginButton() {
+    return (
+        <>
+            <button type="button"></button>
+        </>
+    )
+}
