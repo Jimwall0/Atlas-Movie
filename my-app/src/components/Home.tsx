409e720bd898ce2e052d7bd6d455f12a12db7714
Tsimmuaj Yang <tyang4204@gmail.com>
2026-03-19 16:56:22 +0000

    Figured out routing

--- my-app/src/components/Home.tsx
+++ my-app/src/components/Home.tsx
@@ -1,8 +1,11 @@
-import LoginAuth from "./LoginAuth";
+import Logout from "./Logout";
+import MovieView from "./MovieView";
 export default function Home() {
     return (
         <div>
-            <LoginAuth/>
+            <h1>Homepage</h1>
+            <MovieView/>
+            <Logout/>
         </div>
     )
 }
---
+++ my-app/src/components/MovieView.tsx
@@ -0,0 +1,14 @@
+export default function MovieView(){
+    return (
+        <>
+            <ul>
+                <li>
+                    <p>Shrek</p>
+                </li>
+                <li>
+                    <p>Spy kids</p>
+                </li>
+            </ul>
+        </>
+    )
+}
